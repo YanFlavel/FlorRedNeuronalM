19c8b7053b68a546db46881b9493cc8dcfbc6026
YanFlavel <42754836+YanFlavel@users.noreply.github.com>
2019-04-28 21:16:56 -0500

    primer commit

---
+++ CODIGOFLOR.py
@@ -0,0 +1,205 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+X, Y = load_planar_dataset()
+
+plt.scatter(X[0, :], X[1, :], c=Y[0], s=40, cmap=plt.cm.Spectral);
+plt.show()
+shape_X = X.shape
+shape_Y = Y.shape
+m = shape_X[1]       # training set size
+
+
+print ('The shape of X is: ' + str(shape_X))
+print ('The shape of Y is: ' + str(shape_Y))
+print ('I have m = %d training examples!' % (m))
+
+
+
+def layer_sizes(X, Y):
+    n_x = X.shape[0] 
+    n_h = 4
+    n_y = Y.shape[0] 
+    
+    return (n_x, n_h, n_y)
+
+X_assess, Y_assess = layer_sizes_test_case()
+(n_x, n_h, n_y) = layer_sizes(X_assess, Y_assess)
+print("Tamaño de la capa de entrada: n_x = " + str(n_x))
+print("Tamaño de la capa oculta: n_h = " + str(n_h))
+print("Tamaño de la capa de salida: n_y = " + str(n_y))
+
+
+def initialize_parameters(n_x, n_h, n_y):
+   
+    
+    W1 = np.random.randn(n_h,n_x)* 0.01
+    b1 = np.zeros((n_h,1))
+    W2 = np.random.randn(n_y,n_h) * 0.01
+    b2 = np.zeros((n_y,1))
+    
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+
+n_x, n_h, n_y = initialize_parameters_test_case()
+
+parameters = initialize_parameters(n_x, n_h, n_y)
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+
+def forward_propagation(X, parameters):
+   
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    Z1 = np.dot(W1,X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2,A1) + b2
+    A2 = sigmoid(Z2)
+    
+    assert(A2.shape == (1, X.shape[1]))
+    
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    
+    return A2, cache
+
+
+X_assess, parameters = forward_propagation_test_case()
+
+A2, cache = forward_propagation(X_assess, parameters)
+
+
+
+def compute_cost(A2, Y, parameters):
+    
+    
+    m = Y.shape[1] 
+    logprobs = np.multiply(np.log(A2),Y) + np.multiply(np.log(1 - A2),1 - Y)
+    cost = - np.sum(logprobs) * (1 / m) 
+    
+    cost = np.squeeze(cost)     
+    
+    assert(isinstance(cost, float))
+    
+    return cost
+
+A2, Y_assess, parameters = compute_cost_test_case()
+
+print("cost = " + str(compute_cost(A2, Y_assess, parameters)))
+
+
+def backward_propagation(parameters, cache, X, Y):
+    
+    m = X.shape[1]
+    
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+    
+    dZ2= A2 - Y
+    dW2 = 1 / m *(np.dot(dZ2,A1.T))
+    db2 = 1 / m *(np.sum(dZ2,axis = 1,keepdims = True))
+    dZ1 = np.dot(W2.T,dZ2) * (1 - np.power(A1, 2))
+    dW1 = 1 / m *(np.dot(dZ1,X.T))
+    db1 = 1 / m *(np.sum(dZ1,axis = 1,keepdims = True))
+    
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    
+    return grads
+
+parameters, cache, X_assess, Y_assess = backward_propagation_test_case()
+
+grads = backward_propagation(parameters, cache, X_assess, Y_assess)
+print ("dW1 = "+ str(grads["dW1"]))
+print ("db1 = "+ str(grads["db1"]))
+print ("dW2 = "+ str(grads["dW2"]))
+print ("db2 = "+ str(grads["db2"]))
+
+
+def update_parameters(parameters, grads, learning_rate = 1.2):
+    
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    dW1 = grads["dW1"]
+    db1 = grads["db1"]
+    dW2 = grads["dW2"]
+    db2 = grads["db2"]
+    
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+   
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+
+parameters, grads = update_parameters_test_case()
+parameters = update_parameters(parameters, grads)
+
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
